de53e7577f5a90a6d8d601d4b6298288d3a91096
AashiBh <75898142+AashiBh@users.noreply.github.com>
2021-12-04 20:03:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+song = "";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+function setup(){
+    canvas= createCanvas(450, 450);
+    canvas.position(560, 200);
+
+ video = createCapture(VIDEO)
+ video.hide();
+ 
+ poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose' , gotPoses);
+}
+function modelLoaded(){
+    console.log('Posenet is initialized');
+}
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
+function Playy(){
+    song.play()
+    song.setVolume(1);
+    song.rate(1);
+}
+function Stopp(){
+    song.stop();
+}
+function gotPoses(results){
+    if(results.length > 0){
+    console.log(results)
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    rightWristY=results[0].pose.rightWrist.y;
+    }
+        }
